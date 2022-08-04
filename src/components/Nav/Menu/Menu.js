ffec4bc05643ad5a2a1d52b5ce80224281a386e4
Tatyna Kubatova <tatynakubatova74@gmail.com>
2022-08-04 12:10:22 +0600

    Make header responsive

---
+++ src/components/Nav/Menu/Menu.js
@@ -0,0 +1,10 @@
+import './Menu.css';
+import menu from '../../../assets/menu.png';
+
+const Menu = () => {
+  return ( <div className="Menu">
+    <img src={menu}/>
+  </div> );
+}
+
+export default Menu;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,13 +2,14 @@ import classes from "./Nav.module.css";
 import Logo from "./Logo/Logo";
 import NavItem from "./NavItem/NavItem";
 import logo from '../../assets/logo.png';
+import Menu from "./Menu/Menu";
 
 const Nav = () => {
   return (
     <div className={classes.Nav}>
       <Logo logo={logo}/>
       <NavItem />
-      {/* <Menu /> */}
+      <Menu />
     </div>
   );
 }
